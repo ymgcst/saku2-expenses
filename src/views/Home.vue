32e58c3b7bc8555bd94e072c6f0fbcc190e57061
ymgcst <shota.yamaguchi@hopin.co.jp>
2022-05-25 23:53:22 +0900

    init commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    
+    <h1 style="text-align: center">SakuSaku Expenses</h1>
+
+    <button type="button">hello world.</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
