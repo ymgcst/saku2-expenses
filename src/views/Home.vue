80b03df7f324a04f203a9b95e7b6869460f3e9a8
ymgcst <shota.yamaguchi@hopin.co.jp>
2022-05-26 00:53:24 +0900

    Introduction Bootstrap

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,39 @@
     
     <h1 style="text-align: center">SakuSaku Expenses</h1>
 
-    <button type="button">hello world.</button>
+ <div class="container my-5" id="skill">
+      <h2 class= "py-4 text-dark">Skill</h2>
+      <div class="row row-cols-1 row-cols-md-3">
+        <div class="col mb-4">
+          <div class="card">
+            <img src="img/card.jpg" class="card-img-top px-3 mb-3" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Sample</h5>
+              <p class="card-text">SampleSampleSampleSampleSampleSampleSampleSampleSampleSample</p>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-4">
+          <div class="card">
+            <img src="img/card.jpg" class="card-img-top px-3 mb-3" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Sample</h5>
+              <p class="card-text">SampleSampleSampleSampleSampleSampleSampleSampleSampleSample</p>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-4">
+          <div class="card">
+            <img src="img/card.jpg" class="card-img-top px-3 mb-3" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Sample</h5>
+              <p class="card-text">SampleSampleSampleSampleSampleSampleSampleSampleSampleSample</p>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+
   </div>
 </template>
 
